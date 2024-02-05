da0f79d7ce48dc744c92f8c364fea06b652c13e0
dnavmoy <dnavmoy119@g.educaand.es>
2024-02-05 10:13:52 +0100

    pruebo factorial

---
+++ src/main/java/daw/paraProbar.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author daniel
+ */
+public class paraProbar {
+ 
+    
+    public static void main(String[] args) {
+        
+        System.out.println("factorial de 4 "+ factorial(4));
+    }
+    
+    public static int factorial(int numero){
+        int total=1;
+        for(int i=1;i<=numero;i++){
+            total*=i;
+        }
+        return total;
+    }
+}
