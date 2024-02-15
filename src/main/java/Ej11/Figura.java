31ee4234be1800935023d58473cf1d53f56f95fa
dnavmoy <dnavmoy119@g.educaand.es>
2024-02-15 12:45:51 +0100

    ej11 iniciado

---
+++ src/main/java/Ej11/Figura.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ej11;
+
+/**
+ *
+ * @author daniel
+ */
+public class Figura {
+    
+    private double base;
+    private double altura;
+
+    public Figura(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    
+    
+    
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + (int) (Double.doubleToLongBits(this.base) ^ (Double.doubleToLongBits(this.base) >>> 32));
+        hash = 83 * hash + (int) (Double.doubleToLongBits(this.altura) ^ (Double.doubleToLongBits(this.altura) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Figura other = (Figura) obj;
+        if (Double.doubleToLongBits(this.base) != Double.doubleToLongBits(other.base)) {
+            return false;
+        }
+        return Double.doubleToLongBits(this.altura) == Double.doubleToLongBits(other.altura);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Figura{");
+        sb.append("base=").append(base);
+        sb.append(", altura=").append(altura);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+    
+    
+}
---
+++ src/main/java/Ej11/Rectangulo.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ej11;
+
+/**
+ *
+ * @author daniel
+ */
+public class Rectangulo extends Figura{
+
+    public Rectangulo(double base, double altura) {
+        super(base, altura);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Rectangulo{");
+      
+        sb.append('}');
+        return sb.toString();
+    }
+
+    
+    
+    
+    
+    
+    
+}
